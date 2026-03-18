04bf0d610ed64d97007166fb4064f472c867fff7
I love kludge <98ger@mail.ru>
2026-03-18 16:42:52 +1200

    add exception handlers

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+
+from app.exceptions.exception_handler import register_exception_handlers
+
 from app.modules.auth.routes import auth
 
 app = FastAPI()
+
+register_exception_handlers(app)
+
 app.include_router(auth)
---
+++ backend/app/exceptions/exception_handler.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from jwt.exceptions import ExpiredSignatureError, InvalidSignatureError
+
+from app.modules.auth.services import AuthError
+
+
+async def auth_error(request: Request, exc: AuthError):
+    return JSONResponse(status_code=401, content=exc.details)
+
+
+async def signature_expired(request: Request, exc: ExpiredSignatureError):
+    return JSONResponse(status_code=401, content="ExpiredSignatureError")
+
+
+async def signature_invalid(request: Request, exc: InvalidSignatureError):
+    return JSONResponse(status_code=401, content="InvalidSignatureError")
+
+
+def register_exception_handlers(app: FastAPI):
+    app.add_exception_handler(AuthError, auth_error)
+    app.add_exception_handler(ExpiredSignatureError, signature_expired)
+    app.add_exception_handler(InvalidSignatureError, signature_invalid)
