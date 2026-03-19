87b3f246af332d3079bf73a7f65ecf08a4a9bdc1
I love kludge <98ger@mail.ru>
2026-03-19 12:31:35 +1200

    stop fastapi on failed database connection

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,27 @@
+import os
+import signal
+
 from fastapi import FastAPI
+
+from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
 
 from app.exceptions.exception_handler import register_exception_handlers
 
 from app.modules.auth.routes import auth
 
-app = FastAPI()
+from app.db.session import engine
 
-register_exception_handlers(app)
+app = None
 
-app.include_router(auth)
+try:
+    with engine.connect() as connection:
+        connection.execute(text("SELECT 1"))
+    app = FastAPI()
+    register_exception_handlers(app)
+    app.include_router(auth)
+except OperationalError:
+    print("Have no connection to database\n")
+
+    pid = os.getppid()
+    os.kill(pid, signal.SIGINT)
