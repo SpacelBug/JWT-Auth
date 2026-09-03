af16abcc1459bae12c5b4d9b4f71a2f8381e85d7
I love kludge <98ger@mail.ru>
2026-09-03 12:12:56 +1200

    add verify on device logout endpoint

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -69,7 +69,9 @@ async def logout_all(
 
 
 @auth.post("/logout/{device_id}")
-async def logout_device(device_id, db=Depends(get_db)):
+async def logout_device(
+    device_id, user: UserBase = Depends(get_current_user), db=Depends(get_db)
+):
     AuthService.logout_device(device_id, db)
 
     return {"Message": "Successful logout"}
